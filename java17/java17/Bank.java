728a43ae0a93345e49442d66aea33d6fa66435e7
any9785 <49705429+any9785@users.noreply.github.com>
2019-05-14 12:58:07 +0900

    Add files via upload

---
+++ java17/java17/Bank.java
@@ -0,0 +1,8 @@
+package java17;
+
+public class Bank {
+	double rate = 1.0;
+	public void getInterestRate() {
+		
+	}
+}
---
+++ java17/java17/BankUser.java
@@ -0,0 +1,10 @@
+package java17;
+
+public class BankUser {
+	public static void main(String[] args) {
+	
+	BadBank bad = new BadBank();
+	
+	bad.getInterestRate();
+	}
+}
